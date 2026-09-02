2751f9de221979bbf180f2064c33edd7d3ca7a64
Chris Wilson <divclass01@gmail.com>
2026-09-01 22:05:27 -0400

    Gate Lighthouse categories without noisy preset audits

--- lighthouserc.js
+++ lighthouserc.js
@@ -14,7 +14,6 @@ module.exports = {
       }
     },
     assert: {
-      preset: 'lighthouse:no-pwa',
       assertions: {
         'categories:performance': ['error', { minScore: 1 }],
         'categories:accessibility': ['error', { minScore: 1 }],
